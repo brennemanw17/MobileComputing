411889d91f33d746b0748dacb11cc81cb3138161
brennemanw17 <brennemanw17@students.ecu.edu>
2021-05-18 16:20:28 -0400

    Challenges
    Fixing accidental commit to chapter work instead of challenge directory

--- Chapter 2/GeoQuizChallenge/app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
+++ Chapter 2/GeoQuizChallenge/app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
@@ -3,6 +3,7 @@ package com.bignerdranch.android.geoquiz
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageButton
 import android.widget.TextView
 import android.widget.Toast
 
@@ -10,7 +11,8 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var trueButton: Button
     private lateinit var falseButton: Button
-    private lateinit var nextButton: Button
+    private lateinit var nextButton: ImageButton
+    private lateinit var previousButton: ImageButton
     private lateinit var questionTextView: TextView
 
     private val questionBank = listOf(
@@ -31,6 +33,7 @@ class MainActivity : AppCompatActivity() {
         trueButton = findViewById(R.id.true_button)
         falseButton = findViewById(R.id.false_button)
         nextButton = findViewById(R.id.next_button)
+        previousButton = findViewById(R.id.previous_button)
         questionTextView = findViewById(R.id.question_view_text)
 
         trueButton.setOnClickListener { view ->
@@ -41,6 +44,19 @@ class MainActivity : AppCompatActivity() {
         }
 
         nextButton.setOnClickListener {
+            currentIndex = (currentIndex+1) % questionBank.size
+            updateQuestion()
+        }
+
+        previousButton.setOnClickListener {
+            currentIndex = (currentIndex-1)
+            if(currentIndex < 0){
+                currentIndex = questionBank.size - 1
+            }
+            updateQuestion()
+        }
+
+        questionTextView.setOnClickListener {
             currentIndex = (currentIndex+1) % questionBank.size
             updateQuestion()
         }
