f3112801f5a71e027849577ac4c7f37fa0d3e4f9
brennemanw17 <brennemanw17@students.ecu.edu>
2021-05-18 16:03:23 -0400

    Challenge 1 End

--- Chapter 2/GeoQuiz/app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
+++ Chapter 2/GeoQuiz/app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
@@ -45,6 +45,11 @@ class MainActivity : AppCompatActivity() {
             updateQuestion()
         }
 
+        questionTextView.setOnClickListener {
+            currentIndex = (currentIndex+1) % questionBank.size
+            updateQuestion()
+        }
+
         updateQuestion()
     }
 
