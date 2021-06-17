922ff90d1f19c5d839e45f45ef645ebcedecf53b
brennemanw17 <brennemanw17@students.ecu.edu>
2021-06-17 15:06:29 -0400

    chpt 25 init

---
+++ Chapter 25/PhotoGallery/app/src/main/java/com/bignerdranch/android/photogallery/api/FlickrApi.kt
@@ -0,0 +1,16 @@
+package com.bignerdranch.android.photogallery.api
+
+import retrofit2.Call
+import retrofit2.http.GET
+
+interface FlickrApi {
+
+    @GET(
+        "services/rest/?method=flickr.interestingness.getList" +
+                "&api_key=98a176a53e657243a1940f8282725088" +
+                "&format=json" +
+                "&nojsoncallback=1" +
+                "&extras=url_s"
+    )
+    fun fetchPhotos(): Call<FlickrResponse>
+}
